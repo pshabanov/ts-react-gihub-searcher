fcd3ad2e43b50ac20bc22be700ae33815fc4b514
Pavel Shabanov <pshabanov@nurtelecom.kg>
2023-09-29 19:08:19 +0600

    #0006(фича): Компонент поиска + кнопка

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Container } from './components/Container'
 import 'index.css'
 import { Header } from './components/Header'
+import { Search } from './components/Search'
 
 function App() {
   return (
       <Container>
           <Header />
+          <Search hasError onSubmit={()=>{}} />
       </Container>
   );
 }
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+// @ts-ignore
+import styles from './Button.module.scss';
+import React from 'react'
+
+interface ButtonProps {
+    children: string
+    onClick?: React.MouseEventHandler<HTMLButtonElement>
+}
+
+export const Button = ({ children, onClick }: ButtonProps) => (
+  <button className={styles.button} onClick={onClick}>
+      { children }
+  </button>
+);
